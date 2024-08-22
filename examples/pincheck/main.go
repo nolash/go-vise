123cb10f23b057fdc4852c7d152b064c90be4b6f
lash <dev@holbrook.no>
2024-08-22 03:01:19 +0100

    Pin check example

---
+++ examples/pincheck/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path"
+
+	testdataloader "github.com/peteole/testdata-loader"
+
+	"git.defalsify.org/vise.git/cache"
+	"git.defalsify.org/vise.git/engine"
+	"git.defalsify.org/vise.git/resource"
+	"git.defalsify.org/vise.git/state"
+)
+
+const (
+	USERFLAG_VALIDPIN = iota + state.FLAG_USERSTART
+	USERFLAG_INVALIDPIN
+	USERFLAG_QUERYPIN
+)
+
+var (
+	baseDir = testdataloader.GetBasePath()
+	scriptDir = path.Join(baseDir, "examples", "pincheck")
+	pin = []byte("1234")
+)
+
+type pinResource struct{
+	resource.Resource
+	st *state.State
+}
+
+func newPinResource(resource resource.Resource, state *state.State) *pinResource {
+	return &pinResource{
+		resource,
+		state,
+	}
+}
+
+func(rs *pinResource) pinCheck(ctx context.Context, sym string, input []byte) (resource.Result, error) {
+	var r resource.Result
+
+	if rs.st.MatchFlag(USERFLAG_QUERYPIN, false) {
+		r.Content = "Please enter PIN"
+		r.FlagReset = []uint32{USERFLAG_INVALIDPIN, USERFLAG_VALIDPIN}
+		r.FlagSet = []uint32{USERFLAG_QUERYPIN}
+		return r, nil
+	}
+	if bytes.Equal(input, pin) {
+		r.FlagSet = []uint32{USERFLAG_VALIDPIN}
+		engine.Logg.DebugCtxf(ctx, "pin match", "state", rs.st, "rs", r.FlagSet, "rr", r.FlagReset)
+	} else {
+		r.FlagSet = []uint32{USERFLAG_INVALIDPIN}
+		r.Content = "Wrong PIN please try again"
+	}
+	return r, nil
+}
+
+func(rs *pinResource) pinClear(ctx context.Context, sym string, input []byte) (resource.Result, error) {
+	var r resource.Result
+	r.FlagReset = []uint32{USERFLAG_INVALIDPIN, USERFLAG_VALIDPIN, USERFLAG_QUERYPIN}
+	return r, nil
+}
+
+func main() {
+	root := "root"
+	flag.Parse()
+	fmt.Fprintf(os.Stderr, "starting session at symbol '%s' using resource dir: %s\n", root, scriptDir)
+
+	st := state.NewState(3)
+	st.UseDebug()
+	state.FlagDebugger.Register(USERFLAG_VALIDPIN, "VALIDPIN")
+	state.FlagDebugger.Register(USERFLAG_INVALIDPIN, "INVALIDPIN")
+	state.FlagDebugger.Register(USERFLAG_QUERYPIN, "QUERYPIN")
+	rsf := resource.NewFsResource(scriptDir)
+	rs := newPinResource(rsf, &st)
+	rsf.AddLocalFunc("pincheck", rs.pinCheck)
+	rsf.AddLocalFunc("pinclear", rs.pinClear)
+	ca := cache.NewCache()
+	cfg := engine.Config{
+		Root: "root",
+	}
+	ctx := context.Background()
+	en := engine.NewEngine(ctx, cfg, &st, rs, ca)
+	var err error
+	_, err = en.Init(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "engine init fail: %v\n", err)
+		os.Exit(1)
+	}
+
+	err = engine.Loop(ctx, &en, os.Stdin, os.Stdout)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "loop exited with error: %v\n", err)
+		os.Exit(1)
+	}
+}
