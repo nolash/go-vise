8f06e6a16fbf3b2500c651a5a24d95b60dc895a1
lash <dev@holbrook.no>
2024-09-11 03:43:14 +0100

    Test coverage for fsdb and gdbm over 80%

--- db/db_test.go
+++ db/db_test.go
@@ -1,1 +1,13 @@
 package db
+
+func TestDbBase(t *testing.T) {
+	store := NewDbBase()
+	store.SetPrefix(USERDATA_STATE)
+	if !store.Prefix() == USERDATA_STATE {
+		t.Fatal("expected %d, got %d", USERDATA_STATE, store.Prefix())
+	}
+	l, err := store.SetLanguage(lang.LanguageFromCode("nor"))
+	if err != nil {
+		t.Fatal(err)
+	}
+}
