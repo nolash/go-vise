e568a958ec4698066e520f7283560704a3147dec
lash <dev@holbrook.no>
2024-12-11 19:16:16 +0000

    Add postgres internals docs

--- db/postgres/doc.go
+++ db/postgres/doc.go
@@ -1,2 +1,4 @@
 // Package postgres is a Postgres database backed implementation of the db.Db interface.
+//
+// The postgres implementation of the vise key-value store uses two data columns of type `BYTEA` for each key and value, aswell as an `updated` field of type `TIMESTAMP` that is set to the current time when an update is made.that is set to the current time when an update is made.
 package postgres
