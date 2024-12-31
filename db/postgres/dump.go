35b41792f61b43cf97071ba94d1edc3f6ccc51cb
lash <dev@holbrook.no>
2024-12-31 08:28:05 +0000

    Remove premature close

--- db/postgres/dump.go
+++ db/postgres/dump.go
@@ -15,23 +15,23 @@ func(pdb *pgDb) Dump(ctx context.Context, key []byte) (*db.Dumper, error) {
 
 	k := append([]byte{db.DATATYPE_USERDATA}, key...)
 
-	query := fmt.Sprintf("SELECT key, value FROM %s.kv_vise WHERE key >= $1 AND key < $2", pdb.schema)
+	query := fmt.Sprintf("SELECT key, value FROM %s.kv_vise WHERE key >= $1", pdb.schema)
 	logg.TraceCtxf(ctx, "getkey", "q", query, "key", k)
-	rs, err := tx.Query(ctx, query, k, []byte{k[0] + 1})
+	rs, err := tx.Query(ctx, query, k)
 	if err != nil {
 		logg.Debugf("query fail", "err", err)
 		tx.Rollback(ctx)
 		return nil, err
 	}
-	defer rs.Close()
+	//defer rs.Close()
 
 	if rs.Next() {
 		r := rs.RawValues()
-		tx.Commit(ctx)
 		//tx.Rollback(ctx)
+		tx.Commit(ctx)
 		pdb.it = rs
 		pdb.itBase = k
-		return db.NewDumper(pdb.dumpFunc).WithFirst(r[0], r[1]), nil
+		return db.NewDumper(pdb.dumpFunc).WithClose(pdb.closeFunc).WithFirst(r[0], r[1]), nil
 	}
 
 	return nil, db.NewErrNotFound(k)
@@ -39,10 +39,19 @@ func(pdb *pgDb) Dump(ctx context.Context, key []byte) (*db.Dumper, error) {
 
 func(pdb *pgDb) dumpFunc(ctx context.Context) ([]byte, []byte) {
 	if !pdb.it.Next() {
+		logg.DebugCtxf(ctx, "no more data in pg iterator")
 		pdb.it = nil
 		pdb.itBase = nil
 		return nil, nil
 	}
 	r := pdb.it.RawValues()
 	return r[0], r[1]
+}
+
+func(pdb *pgDb) closeFunc() error {
+	if pdb.it != nil {
+		pdb.it.Close()
+		pdb.it = nil
+	}
+	return nil
 }
